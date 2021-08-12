3be5a51486bf3538143bbf1a2ae5d4c3836985ac
Vivek Kumar Tiwari <vivekkumar.tiwari.ext@wsa.com>
2021-08-12 11:35:29 +0530

    Add project files.

---
+++ Movie.API/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MoviesController : ControllerBase
+    {
+        public static IEnumerable<MovieEntity> Movies { get; set; }
+
+        public MoviesController()
+        {
+            Movies = new List<MovieEntity>
+            {
+                new MovieEntity { Id = 1, Title = "Toy Story 1", Director = "John Lasseter" },
+                new MovieEntity { Id = 2, Title = "Toy Story 4", Director = "Josh Cooley" },
+                new MovieEntity { Id = 3, Title = "Arrival", Director = "Denis Villeneuve" },
+                new MovieEntity { Id = 4, Title = "Interstellar", Director = "Christopher Nolan" },
+                new MovieEntity { Id = 5, Title = "The Martian", Director = "Ridley Scott" },
+
+                new MovieEntity { Id = 6, Title = "Avatar", Director = "James Cameron" },
+                new MovieEntity { Id = 7, Title = "Prometheus", Director = "Ridley Scott" },
+                new MovieEntity { Id = 8, Title = "Sunshine", Director = "Danny Boyle" },
+
+                new MovieEntity { Id = 9, Title = "Serenity", Director = "Joss Whedon" },
+                new MovieEntity { Id = 10, Title = "WALL-E", Director = "Andrew Stanton" },
+            };
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Movies);
+        }
+    }
+
+    public class MovieEntity
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+    }
+}
---
+++ Movies.Api.Test/MovieControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using Movie.API.Controllers;
+using Xunit;
+
+namespace Movies.Api.Test
+{
+    public class MovieControllerTest
+    {
+        [Fact]
+        public void Get_Test()
+        {
+            MoviesController controller = new MoviesController();
+            var result = controller.Get();
+            Assert.NotNull(result);
+        }
+    }
+}
